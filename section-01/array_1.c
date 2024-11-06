4be3fa621f984e8ab1769fe5ff58abbdace7fa15
刘闽 <liumin@hengzhiinfo.com>
2024-11-06 09:56:05 +0800

    feat: array_1

---
+++ section-01/array_1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(int argc, char** argv) {
+    int arr[10];
+    for (int i = 0; i < 10; i++) {
+        printf("%d \t", arr[i]);
+    }
+    printf("\n");
+
+    return 0;
+}
